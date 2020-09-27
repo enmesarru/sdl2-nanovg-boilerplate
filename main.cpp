99177ce51c6b8a4965291f2737fed8f50841dae4
Furkan KURUTAS <fkurutass@gmail.com>
2020-09-27 14:32:28 +0300

    draw curve test

--- main.cpp
+++ main.cpp
@@ -36,6 +36,77 @@ using namespace gl;
 #include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
 #endif
 
+void drawGraph(NVGcontext* vg, float x, float y, float w, float h, float t)
+{
+	NVGpaint bg;
+	float samples[6];
+	float sx[6], sy[6];
+	float dx = w/5.0f;
+	int i;
+
+	samples[0] = (1+sinf(t*1.2345f+cosf(t*0.33457f)*0.44f))*0.5f;
+	samples[1] = (1+sinf(t*0.68363f+cosf(t*1.3f)*1.55f))*0.5f;
+	samples[2] = (1+sinf(t*1.1642f+cosf(t*0.33457)*1.24f))*0.5f;
+	samples[3] = (1+sinf(t*0.56345f+cosf(t*1.63f)*0.14f))*0.5f;
+	samples[4] = (1+sinf(t*1.6245f+cosf(t*0.254f)*0.3f))*0.5f;
+	samples[5] = (1+sinf(t*0.345f+cosf(t*0.03f)*0.6f))*0.5f;
+
+	for (i = 0; i < 6; i++) {
+		sx[i] = x+i*dx;
+		sy[i] = y+h*samples[i]*0.8f;
+	}
+
+	// Graph background
+	bg = nvgLinearGradient(vg, x,y,x,y+h, nvgRGBA(0,160,192,0), nvgRGBA(0,160,192,64));
+	nvgBeginPath(vg);
+	nvgMoveTo(vg, sx[0], sy[0]);
+	for (i = 1; i < 6; i++)
+		nvgBezierTo(vg, sx[i-1]+dx*0.5f,sy[i-1], sx[i]-dx*0.5f,sy[i], sx[i],sy[i]);
+	nvgLineTo(vg, x+w, y+h);
+	nvgLineTo(vg, x, y+h);
+	nvgFillPaint(vg, bg);
+	nvgFill(vg);
+
+	// Graph line
+	nvgBeginPath(vg);
+	nvgMoveTo(vg, sx[0], sy[0]+2);
+	for (i = 1; i < 6; i++)
+		nvgBezierTo(vg, sx[i-1]+dx*0.5f,sy[i-1]+2, sx[i]-dx*0.5f,sy[i]+2, sx[i],sy[i]+2);
+	nvgStrokeColor(vg, nvgRGBA(0,0,0,32));
+	nvgStrokeWidth(vg, 3.0f);
+	nvgStroke(vg);
+
+	nvgBeginPath(vg);
+	nvgMoveTo(vg, sx[0], sy[0]);
+	for (i = 1; i < 6; i++)
+		nvgBezierTo(vg, sx[i-1]+dx*0.5f,sy[i-1], sx[i]-dx*0.5f,sy[i], sx[i],sy[i]);
+	nvgStrokeColor(vg, nvgRGBA(0,160,192,255));
+	nvgStrokeWidth(vg, 3.0f);
+	nvgStroke(vg);
+
+	// Graph sample pos
+	for (i = 0; i < 6; i++) {
+		bg = nvgRadialGradient(vg, sx[i],sy[i]+2, 3.0f,8.0f, nvgRGBA(0,0,0,32), nvgRGBA(0,0,0,0));
+		nvgBeginPath(vg);
+		nvgRect(vg, sx[i]-10, sy[i]-10+2, 20,20);
+		nvgFillPaint(vg, bg);
+		nvgFill(vg);
+	}
+
+	nvgBeginPath(vg);
+	for (i = 0; i < 6; i++)
+		nvgCircle(vg, sx[i], sy[i], 4.0f);
+	nvgFillColor(vg, nvgRGBA(0,160,192,255));
+	nvgFill(vg);
+	nvgBeginPath(vg);
+	for (i = 0; i < 6; i++)
+		nvgCircle(vg, sx[i], sy[i], 2.0f);
+	nvgFillColor(vg, nvgRGBA(220,220,220,255));
+	nvgFill(vg);
+
+	nvgStrokeWidth(vg, 1.0f);
+}
+
 int main(int, char**)
 {
     constexpr unsigned int WIDTH = 1280;
@@ -108,14 +179,17 @@ int main(int, char**)
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImVec4 clear_color = ImVec4(0.f, 0.f, 0.f, 1.00f);
     NVGcontext *vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
     
     float pxRatio = (float)WIDTH / (float)HEIGHT;
     // Main loop
     bool done = false;
+    int counter = 0;
+
     while (!done)
     {
+        double time;
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -125,45 +199,35 @@ int main(int, char**)
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
                 done = true;
         }
-
+        time = SDL_GetTicks();
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame(window);
         ImGui::NewFrame();
 
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        ImGui::Begin("Load File");                          // Create a window called "Hello, world!" and append into it.
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
 
-            ImGui::Begin("Load File");                          // Create a window called "Hello, world!" and append into it.
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
         // Rendering
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        {
-            // Render NanoVg
-            nvgBeginFrame(vg, WIDTH, HEIGHT, pxRatio);
 
-                nvgBeginPath(vg);
-                nvgRect(vg, 300,100, 120,30);
-                nvgFillColor(vg, nvgRGBA(255,192,0,255));
-                nvgFill(vg);
-            nvgEndFrame(vg);
-            // Nanovg
-        }
+
+        nvgBeginFrame(vg, WIDTH, HEIGHT, pxRatio);
+	    drawGraph(vg, 0, HEIGHT/2, WIDTH, HEIGHT/2, time / 1000 * counter);
+        nvgEndFrame(vg);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
     }
+
+	nvgDeleteGL3(vg);
 
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
@@ -176,3 +240,4 @@ int main(int, char**)
 
     return 0;
 }
+
